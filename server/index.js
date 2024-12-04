a36a0635d749b783f1b3f1d81a65c64f74d11eec
guru1121 <gurunandm@gmail.com>
2024-12-04 16:30:26 +0530

    update

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const app = express();
 app.use("/", (req, res)=>{
      res.send("hello world")
 })
-app.listen(5001, console.log(" app is listen on port 5001") );
+// app.listen(5001, console.log(" app is listen on port 5001") );
 
 module.exports = (req, res) => app(req, res);
