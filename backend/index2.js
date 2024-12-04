d4eab65f6264ebc9da883a36ead2e67044b81678
guru1121 <gurunandm@gmail.com>
2024-12-04 17:12:25 +0530

    vercel update

--- backend/index2.js
+++ backend/index2.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const userRoutes = require("./routes/userRoutes");
-require("dotenv").config();
-const cors = require("cors");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const userRoutes = require("./routes/userRoutes");
+// require("dotenv").config();
+// const cors = require("cors");
 
-const app = express();
-const PORT = 5000;
+// const app = express();
+// const PORT = 5000;
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
+// // Middleware
+// app.use(cors());
+// app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("MongoDB connection error:", err));
+// // Connect to MongoDB
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.log("MongoDB connection error:", err));
 
-// Routes
-app.use("/api/users", userRoutes);
+// // Routes
+// app.use("/api/users", userRoutes);
 
-// Start Server
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// // Start Server
+// app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
