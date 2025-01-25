dbefba7aadad0c81490d769a6f2c23bc920a5027
guru1121 <gurunandm@gmail.com>
2025-01-25 16:48:24 +0530

    one

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = 5001
 
 app.use("/", (req, res)=>{
-     res.send("Hello world")
+     res.send("Hello world1")
 })
- app.listen(5001, console.log(`Server running on http://localhost:${PORT}`) );
+ app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`) );
 
 module.exports = (req, res) => app(req, res);
