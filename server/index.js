ec1939103681c3e6d38bea7872d36509e633a567
guru1121 <gurunandm@gmail.com>
2025-01-25 16:45:27 +0530

    capital

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const PORT = 5001
 
 app.use("/", (req, res)=>{
-     res.send("hello world")
+     res.send("Hello world")
 })
-// app.listen(5001, console.log(" app is listen on port 5001") );
+ app.listen(5001, console.log(`Server running on http://localhost:${PORT}`) );
 
 module.exports = (req, res) => app(req, res);
