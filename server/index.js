42bfabd6ef45cc1b72918456935e2b6e0565574b
guru1121 <gurunandm@gmail.com>
2025-01-26 19:44:25 +0530

    pass sec

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ mongoose
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  password: {type: String, required:true }
 });
 
 // Create a model for the 'api_data' collection in MongoDB
